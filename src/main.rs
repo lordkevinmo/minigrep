ef97a4b91ef59f0cc124892c8756048da76b2944
Moise Agbenya <moise.agbenya@gmail.com>
2023-08-13 19:58:06 +0200

    feat: add search feature and unit test to the search

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::{env, process};
 
 use minigrep::{Config, run};
 
-
-
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -11,9 +9,6 @@ fn main() {
         println!("Encounter an issue while parsing arguments: {err}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
         println!("Application error: {e}");
